2f54af0f68a1fb7a7e6e0f328c19091c29688950
604234030cs <604234030@parichat.skru.ac.th>
2020-12-14 21:58:08 +0700

    102

---
+++ src/pages/enums/enums.ts
@@ -0,0 +1,7 @@
+export enum APIURL{
+
+    URL = "http://localhost/todoslim3"
+    // URL = "http://192.168.43.83/todoslim3"
+    // URL = "http://10.49.8.32/todoslim3"
+    // URL = "http://projectend.herokuapp.com"
+}
